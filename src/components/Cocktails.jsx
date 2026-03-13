7c0f7ce9112459bfb483d32a9909c5f89fb1e03f
Hritvik Singh Bedi <hellohritvik@gmail.com>
2026-03-13 22:45:49 +0530

    Started Cocktails Section

---
+++ src/components/Cocktails.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Cocktails = () => {
+  return (
+    <section id="cocktails" className = "Noisy">
+        <img src="/images/cocktail-left-leaf.png" alt="l-leaf" id="c-left-leaf" />
+        <img src="/images/cocktail-right-leaf.png" alt="r-leaf" id="c-right-leaf" />
+
+        
+    </section>
+  )
+}
+
+export default Cocktails
