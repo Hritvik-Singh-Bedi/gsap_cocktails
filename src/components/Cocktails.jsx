1ed921b22d087fa14b2a9c7f3cf8a2e573d5f53c
Hritvik Singh Bedi <hellohritvik@gmail.com>
2026-03-21 16:02:24 +0530

    Implemented the About Section

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -26,7 +26,7 @@ const Cocktails = () => {
     })
   });
   return (
-    <section id="cocktails" className = "Noisy">
+    <section id="cocktails" className = "noisy">
         <img src="/images/cocktail-left-leaf.png" alt="l-leaf" id="c-left-leaf" />
         <img src="/images/cocktail-right-leaf.png" alt="r-leaf" id="c-right-leaf" />
 
